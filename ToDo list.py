83b96cc216454ec09975f5cc88c5eaa2811f4048
Janvier <107079015+Janvier46@users.noreply.github.com>
2022-06-20 10:00:09 +0000

    Initial commit

---
+++ ToDo list.py
@@ -0,0 +1,22 @@
+task_dict = {} # Создаем пустой словарь, в который затем будем добавлять записи
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+print(date, task)
+print(task_dict)
+
+# Если хотите лучше понять это решение - добавьте вывод на экран после каждого пользовательского ввода
+# print(date, task)
+# print(task_dict)
+# Вы увидите, что переменные date и task изменяются, но данные в словаре task_dict остаются неизменными
